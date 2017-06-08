3cc0f2c58240b01ebf834a3b4ed1493ff21c0aed
Timothy Armes <tim@timothyarmes.com>
2017-06-08 15:44:19 +0200

    Issue #16 - Perfectly center text in RoundedButtonCell

--- FontYou/RoundedButton.swift
+++ FontYou/RoundedButton.swift
@@ -19,6 +19,24 @@ class RoundedButtonCell: NSButtonCell {
             NSBezierPath(roundedRect: frame, xRadius: frame.height / 2, yRadius: frame.height / 2).fill()
         }
     }
+    
+    override func drawInterior(withFrame cellFrame: NSRect, in controlView: NSView) {
+        
+        // Get the actual bounding box of the glyphs using CoreText
+        
+        let line = CTLineCreateWithAttributedString(attributedTitle)
+        let runs = CTLineGetGlyphRuns(line) as! [CTRun]
+        let bb = CTRunGetImageBounds(runs[0], nil, CFRangeMake(0, 0))
+        
+        let height = bb.size.height - bb.origin.y
+        
+        // Adjust the frame to center the text vertically
+        
+        var newRect = cellFrame
+        newRect.origin.y = newRect.size.height + bb.origin.y - (cellFrame.height - height) / 2
+        
+        attributedTitle.draw(with: newRect, options: [])
+    }
 }
 
 @IBDesignable
@@ -50,7 +68,6 @@ class RoundedButton: NSButton {
         
         let attributedTitle = NSAttributedString(string: title, attributes: attributes)
         self.attributedTitle = attributedTitle
-
     }
 }
 
