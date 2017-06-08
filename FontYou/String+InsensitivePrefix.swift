1260efd2d27d41f6ae7eb3128f0d718f40433f9c
Timothy Armes <tim@timothyarmes.com>
2017-06-08 11:39:42 +0200

    Issue #28 - Restrict search to start of font name only

---
+++ FontYou/String+InsensitivePrefix.swift
@@ -0,0 +1,9 @@
+//
+//  NSString+InsensitivePrefix.swift
+//  Fontstore
+//
+//  Created by Timothy Armes on 08/06/2017.
+//  Copyright © 2017 Arctic Whiteness. All rights reserved.
+//
+
+import Foundation
